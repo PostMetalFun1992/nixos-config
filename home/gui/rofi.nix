b2848502eb4623aefac71cdae26af3e9846c9777
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-01-22 00:53:40 +0500

    Moved rofi to stylix

--- home/gui/rofi.nix
+++ home/gui/rofi.nix
@@ -1,10 +1,35 @@
-{ pkgs, config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   home.packages = with pkgs; [
     rofi
+    papirus-icon-theme
   ];
 
-  xdg.configFile."rofi/config.rasi".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/dotfiles/rofi/config.rasi";
+  programs.rofi = {
+    enable = true;
+
+    terminal = "${pkgs.alacritty}/bin/alacritty";
+    location = "center";
+
+    font = lib.mkForce "${config.stylix.fonts.monospace.name} 14";
+
+    extraConfig = {
+      modi = "drun";
+      show-icons = true;
+      icon-theme = "Papirus";
+      drun-display-format = "{icon} {name}";
+      disable-history = false;
+      hide-scrollbar = true;
+      display-drun = "   Apps ";
+      sidebar-mode = true;
+    };
+  };
+
+  stylix.targets.rofi.enable = true;
 }
--- home/terminal/alacritty.nix
+++ home/terminal/alacritty.nix
@@ -7,6 +7,7 @@
 
   programs.alacritty = {
     enable = true;
+
     settings = {
       window = {
         padding = {
@@ -16,6 +17,7 @@
         title = "Alacritty";
         decorations = "None";
       };
+
       font = {
         size = lib.mkForce 8.5;
         offset = {
