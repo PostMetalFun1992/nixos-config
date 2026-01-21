1738635bdd541048f68f92278df84d5df2e8c6e3
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-01-22 01:37:12 +0500

    Moved dunst to stylix

--- home/gui/dunst.nix
+++ home/gui/dunst.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
 {
   home.packages = with pkgs; [
@@ -8,8 +13,41 @@
 
   services.dunst = {
     enable = true;
+
+    settings = {
+      global = {
+        monitor = 0;
+        follow = "mouse";
+        width = 300;
+        height = 200;
+        origin = "top-right";
+        offset = "20x20";
+        transparency = 0;
+        padding = 15;
+        horizontal_padding = 15;
+        frame_width = 2;
+        gap_size = 8;
+        separator_height = 2;
+        separator_color = lib.mkForce "frame";
+        font = lib.mkForce "${config.stylix.fonts.monospace.name} 10";
+        format = "<b>%s</b>\\n%b";
+        icon_position = "left";
+        max_icon_size = 48;
+      };
+
+      urgency_low = {
+        timeout = 5;
+      };
+
+      urgency_normal = {
+        timeout = 10;
+      };
+
+      urgency_critical = {
+        timeout = 0;
+      };
+    };
   };
 
-  xdg.configFile."dunst/dunstrc".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/dotfiles/dunst/dunstrc";
+  stylix.targets.dunst.enable = true;
 }
