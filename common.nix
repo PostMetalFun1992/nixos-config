febc690504904cfe9932791b5ae3481a7b7292cd
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-01-22 18:46:39 +0500

    Fixed audio issues

--- common.nix
+++ common.nix
@@ -36,7 +36,18 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    extraConfig.pipewire."99-input-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [ 48000 ];
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
+        "default.clock.max-quantum" = 4096;
+      };
+    };
   };
+  security.rtkit.enable = true;
 
   services.displayManager.ly.enable = true;
 
@@ -99,8 +110,6 @@
       dates = "weekly";
     };
   };
-
-  security.rtkit.enable = true;
 
   users.users.postmetalfun1992 = {
     isNormalUser = true;
