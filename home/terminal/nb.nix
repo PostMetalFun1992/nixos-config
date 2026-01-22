0356651b5a07e8fca8566d0a36f51b35fca4ff7c
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-01-22 15:53:25 +0500

    Moved nb to nix config

--- home/terminal/nb.nix
+++ home/terminal/nb.nix
@@ -7,6 +7,12 @@
     nb
   ];
 
-  home.file.".nbrc".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/dotfiles/nb/.nbrc";
+  home.sessionVariables = {
+    EDITOR = "hx";
+    NB_AUTO_SYNC = "0";
+    NB_GIT_AUTO_COMMIT = "1";
+
+    NB_MARKDOWN_TOOL = "${pkgs.glow}/bin/glow";
+    NB_DIRECTORY_TOOL = "${pkgs.eza}/bin/eza";
+  };
 }
