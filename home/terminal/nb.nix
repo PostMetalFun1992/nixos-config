690fccd057716adba4dc9d1b63469a568d578f10
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-01-22 18:35:01 +0500

    Added auto-sync daemon for nb

--- home/terminal/nb.nix
+++ home/terminal/nb.nix
@@ -15,4 +15,32 @@
     NB_MARKDOWN_TOOL = "${pkgs.glow}/bin/glow";
     NB_DIRECTORY_TOOL = "${pkgs.eza}/bin/eza";
   };
+
+  # Check: systemctl --user list-timers --all | grep nb
+  # Logs: journalctl --user -u nb-sync -n 20
+  systemd.user.services.nb-sync = {
+    Unit = {
+      Description = "Auto sync nb notes";
+      After = [ "network.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.nb}/bin/nb sync";
+      Nice = 19; # Let the other process go forward
+    };
+  };
+
+  systemd.user.timers.nb-sync = {
+    Unit = {
+      Description = "Timer for nb auto sync";
+    };
+    Timer = {
+      Unit = "nb-sync.service";
+      OnStartupSec = "10s";
+      OnUnitActiveSec = "5m";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
 }
