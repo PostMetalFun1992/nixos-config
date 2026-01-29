d683e91432648f0ae4fa536ed2a378314e3e35dd
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-01-30 02:24:37 +0500

    Moved starship to native config

--- home/terminal/starship.nix
+++ home/terminal/starship.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
@@ -8,4 +8,7 @@
   programs.starship = {
     enable = true;
   };
+
+  xdg.configFile."starship.toml".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/configs/starship/starship.toml";
 }
