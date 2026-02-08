bb117bb5c5937dbe88953727570af7444bcd5763
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-02-08 14:19:16 +0500

    Added dev profile for firefox

--- home/gui/firefox.nix
+++ home/gui/firefox.nix
@@ -8,8 +8,40 @@
   programs.firefox = {
     enable = true;
 
-    profiles.postmetalfun1992 = {
-      isDefault = true;
+    profiles = {
+
+      postmetalfun1992 = {
+        id = 0;
+        name = "postmetalfun1992";
+        isDefault = true;
+        path = "postmetalfun1992";
+      };
+
+      developer = {
+        id = 1;
+        name = "developer";
+        isDefault = false;
+        path = "developer";
+      };
+    };
+  };
+
+  xdg.desktopEntries = {
+    firefox-developer-profile = {
+      name = "Firefox Developer Profile";
+      genericName = "Firefox for dev";
+      comment = "Separate profile for Develop & Security research";
+
+      exec = "firefox -P developer --no-remote";
+
+      terminal = false;
+      categories = [
+        "Application"
+        "Network"
+        "WebBrowser"
+      ];
+
+      icon = "firefox";
     };
   };
 }
